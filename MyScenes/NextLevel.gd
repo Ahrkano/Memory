a7f4236267cce09d2e405c3e2f3cb1e7cb252407
Ahrkano <36827623+Ahrkano@users.noreply.github.com>
2019-09-01 22:29:28 -0300

    -next level script- bug fixed

--- MyScenes/NextLevel.gd
+++ MyScenes/NextLevel.gd
@@ -2,5 +2,4 @@ extends Area2D
 
 func _on_NextLevel_body_entered(body):
 	if body.is_in_group("Player"):
-		global.scene = get_node("res://MyScenes/Level 01.tscn")
 		get_tree().change_scene("res://MyScenes/Level 02.tscn")
--- MyScenes/Next_Level_2.gd
+++ MyScenes/Next_Level_2.gd
@@ -2,5 +2,4 @@ extends Area2D
 
 func _on_Next_Level_body_entered(body):
 	if body.is_in_group("Player"):
-		global.scene = get_node("res://MyScenes/Level 02.tscn")
 		get_tree().change_scene("res://MyScenes/Level 03.tscn")
